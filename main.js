bb99b2feb695e76d9d5a1254c8eb697ae807aaae
Geofat <geofat2015@gmail.com>
2023-09-21 11:20:17 +0100

    change function to arrow function

---
+++ main.js
@@ -0,0 +1,8 @@
+var Name = "Joab"
+var WeightInKG ="65" 
+const AboutMe = (Name,WeightInPounds) => {
+    let bio = Name + WeightInPounds;
+    return bio
+}
+console.log (AboutMe(Name,WeightInKG * 2.20462))
+
