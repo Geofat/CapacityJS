e887ac57b4d9ad706edbae910852113d36ec6f2e
Geofat <geofat2015@gmail.com>
2023-09-21 11:51:46 +0100

    edit user to be able to gettheir name and real weight in pounds

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var Name = "Joab"
 var WeightInKG ="65" 
 const AboutMe = (Name,WeightInPounds) => {
-    let bio = Name + WeightInPounds;
+    let bio = yourName + WeightInPounds ;
     return bio
 }
 const yourName = prompt("enter your name")
 const yourWeightInKG = parseFloat(prompt("enter your weight in KG"))
- alert(AboutMe(Name,WeightInKG * 2.20462));
+ alert(AboutMe(Name,yourWeightInKG * 2.20462));
 
